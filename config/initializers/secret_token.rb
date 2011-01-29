6f8d30c426b321804192298a6e69ebcaa7f1e582
simaob <simaobelchior@gmail.com>
2011-01-29 11:15:01 +0000

    First commit. Hello World! =)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GmapsCodejam::Application.config.secret_token = '0e31ae5ddadbb98485c268c84a1fad7eebfca84d137effd84ec9dac7d9069ddc9aa33106d198900ff96c5ba559f4ddcdf2ed806659f0783cfa76f31c5ed1a342'
